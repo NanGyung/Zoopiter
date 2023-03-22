f5bc37986d162e7b387bb179fd13f2fc9e92a8a8
NanGyung <rhrnakaktdidrod0926@gmail.com>
2023-03-22 23:33:38 +0900

    새로운 캘린더 라이브러리

--- js/mypage_scheduler.js
+++ js/mypage_scheduler.js
@@ -16,6 +16,7 @@ function calendarInit() {
   var kstGap = 9 * 60 * 60 * 1000; // 한국 kst 기준시간 더하기
   var today = new Date(utc + kstGap); // 한국 시간으로 date 객체 만들기(오늘)
 
+  //한국 현재 년,월,일 date객체
   var thisMonth = new Date(
     today.getFullYear(),
     today.getMonth(),
@@ -30,8 +31,12 @@ function calendarInit() {
   // kst 기준 현재시간
   // console.log(thisMonth);
 
+  // var currentMonthDate = new Map();
+
+
   // 캘린더 렌더링
   renderCalender(thisMonth);
+
 
   function renderCalender(thisMonth) {
     // 렌더링을 위한 데이터 정리
@@ -54,6 +59,8 @@ function calendarInit() {
     // 현재 월 표기
     $('.year-month').text(currentYear + '.' + (currentMonth + 1));
 
+    const dateArr = [];
+
     // 렌더링 html 요소 생성
     calendar = document.querySelector('.dates');
     calendar.innerHTML = '';
@@ -74,31 +81,35 @@ function calendarInit() {
         calendar.innerHTML + '<div class="day next disable">' + i + '</div>';
     }
 
+    // var days = document.querySelectorAll('.dates .day.current');
+    // let daysArr = [];
+    // for (const value of days.values()) {
+    //   daysArr.push(value);
+    //   // console.log(value);
+    // }
     // 오늘 날짜 표기
     if (today.getMonth() == currentMonth) {
       todayDate = today.getDate();
-      var currentMonthDate = document.querySelectorAll('.dates .current');
-      currentMonthDate[todayDate - 1].classList.add('today');
+      var currentMonthDate = document.querySelectorAll('.dates .day.current');
+      dateArr.push(Object.values(currentMonthDate));
+      dateArr[0][todayDate - 1].classList.add('today');
     }
 
-    //currentMonthDate : NodeList 객체에 접근 해야함
-    console.log(currentMonthDate.values());
-    // 날짜 선택
-    for(const ele of currentMonthDate){
-      // ele는 날짜(일)태그 <div class="day current">1...31</div>
-      // var values = ele.innerHTML;
-      // console.log(values); // 1...31
+    // console.log(Object.values(currentMonthDate));
 
+    // 날짜 선택
+    for (const ele of dateArr[0]) {
       ele.addEventListener('click', e => {
-        const todayItem = document.querySelector('.current.today'); 
         const clickItem = e.target;
+        const todayItem = document.querySelector('.current.today');
         console.log(clickItem);
         // console.log(e.target.innerHTML); //클릭한 날짜의 값
+
         //현재 날짜 표시하는 div의 class 값 제거
         todayItem.classList.remove('today');
         // 선택한 날짜 표시
         clickItem.classList.add('today');
-          
+
       }, false);
     }
   }
@@ -109,7 +120,6 @@ function calendarInit() {
   $('.go-prev').on('click', function () {
     thisMonth = new Date(currentYear, currentMonth - 1, 1);
     renderCalender(thisMonth);
-    todayItem.classList.remove('today');
   });
 
   // 다음달로 이동
