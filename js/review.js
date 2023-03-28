6054c2fcec9b185d404dc59da14bf61d13020cc3
“NanGyung” <rhrnakaktdidrod0926@gmail.com>
2023-03-28 17:33:29 +0900

    병원후기 체크박스 완료

--- js/review.js
+++ js/review.js
@@ -1,6 +1,6 @@
 // 동물 태그 셀렉트 박스 이벤트
 
-// 선택한 태그를 추가할 영역
+// // 선택한 태그를 추가할 영역
 // const $tagList = document.querySelector('.tag-list');
 // // 동물유형 선택 셀렉트 박스
 // const $select = document.querySelector('select[name="pet"]');
@@ -28,20 +28,24 @@
 
 const $selectBox = document.querySelector('.left .selectBox');
 const $check = document.querySelectorAll('input[type="checkbox"]');
+let addBtn;
 
 const  checkbox_h = e => {
+    let checkedCnt = 0;
+
     // 동물태그 체크박스 선택했을 시 전체 선택 해제 버튼 추가
     for(const ele of $check){
         if (ele.checked == true) {
-            console.log(e.target);
-            let addBtn = document.createElement("button");
-            addBtn.innerHTML = '전체 선택 해제';
-            addBtn.setAttribute("id", "allBtn");
-            $selectBox.appendChild(addBtn);
-        } else {
-            // const $addBtn = document.getElementById('allBtn');
-            $selectBox.removeChild(addBtn);
+            checkedCnt++;
         }
+    }
+    if(checkedCnt === 1){
+        addBtn = document.createElement("button");
+        addBtn.innerHTML = '전체 선택 해제';
+        addBtn.setAttribute("id", "allBtn");
+        $selectBox.appendChild(addBtn);
+    }else if(checkedCount === 0 || checkedCount === $check.length){
+        $selectBox.removeChild(addBtn);
     }
 }
 
